767ef21a90c6b6a997cbd4882db5fd3620304067
JorgeOsiris00 <jorgeosiris9@gmail.com>
2023-02-18 12:27:14 +0100

    creacion del esqueleto BIEN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './componentes/menu/menu/menu.component';
-import { FooterComponent } from './componentes/footer/footer/footer.component';
-import { TablaComponent } from './componentes/tabla/tabla/tabla.component';
-import { CardComponent } from './componentes/card/card/card.component';
-import { FormComponent } from './componentes/form/form/form.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { TablaComponent } from './componentes/tabla/tabla.component';
+import { CardComponent } from './componentes/card/card.component';
+import { FormComponent } from './componentes/form/form.component';
 
 @NgModule({
   declarations: [
